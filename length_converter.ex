75493a272dcfcc1aa9ea04808b367df65182e708
tosbourn <toby.osbourn@googlemail.com>
2017-06-08 13:40:29 +0100

    Page 19

--- length_converter.ex
+++ length_converter.ex
@@ -1,13 +1,7 @@
 defmodule MeterToLengthConverter do
   defmodule Feet do
-    def convert(m) do
-      m * 3.28084
-    end
-  end
-
-  defmodule Inch do
-    def convert(m) do
-      m * 39.3701
-    end
+    def convert(:feet, m), do: m * 3.28084
+    def convert(:inch, m), do: m * 39.3701
+    def convert(:yard, m), do: m * 1.09361
   end
 end
